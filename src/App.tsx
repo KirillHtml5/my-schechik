17b37d29c50f4ea4e6a04beef41ec9a9e8fac773
KirillHtml5 <mr.kiril17@icloud.com>
2022-03-14 16:33:45 +0300

    add first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Tablo} from "./component/Tablo";
+import {IncButton} from "./component/IncButton";
+import {ResetButton} from "./component/ResetButton";
+import {UniButton} from "./component/UniButton";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const MaxValue = 5;
+    const StartValue = 0;
+    const [num, setNum] = useState<number>(0)
+    const onClickInc = () => {
+        if (num < MaxValue)
+            setNum(num + 1)
+
+    }
+    const onClickReset = () => {
+        setNum(StartValue)
+    }
+
+    return (
+        <div className={'App'}>
+            <div className={'tab'}>
+                <Tablo number={num}/>
+            </div>
+            <div className={'uni1'}>
+                <UniButton name={'inc'} callBack={onClickInc} disabled={num === MaxValue}/>
+                <UniButton name={'reset'} callBack={onClickReset} disabled={num === StartValue}/>
+                {/*<IncButton onClickInc={onClickInc} number={num}/>*/}
+                {/*<ResetButton onClickReset={onClickReset} number={num}/>*/}
+            </div>
+        </div>
+
+    );
 }
 
 export default App;
