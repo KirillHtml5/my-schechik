8da978b695340dffdacef72e3d0fe3b842f70ca7
KirillHtml5 <mr.kiril17@icloud.com>
2022-03-20 12:21:07 +0300

    add leftTablo1

--- src/component/LeftTablo.tsx
+++ src/component/LeftTablo.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
 export const LeftTablo = () => {
+    let [maxValue, setMaxValue] = useState<number>(5)
+
+    const setLocal = () => {
+        localStorage.setItem('Max value', JSON.stringify(maxValue))
+    }
+    const onChangeMaxHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setMaxValue(e.currentTarget.valueAsNumber)
+    }
     return (
         <div>
-            <span>Max value: <input type={"number"}/></span>
+            <span>
+                Max value:
+                <input
+
+                    type={"number"}
+                    onChange={onChangeMaxHandler}
+                /></span>
             <span>Start value:<input type={"number"}/></span>
-            <button>Set</button>
+            <button onClick={setLocal}>Set</button>
         </div>
     );
 };
